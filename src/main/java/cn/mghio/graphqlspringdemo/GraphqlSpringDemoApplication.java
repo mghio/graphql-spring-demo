55aaaa9e9009a5133641339c4dde59732d0538b4
guihai <guihai@xingren.com>
2021-02-17 10:32:31 +0800

    add GraphQL with SpringBoot Demo case

---
+++ src/main/java/cn/mghio/graphqlspringdemo/GraphqlSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.mghio.graphqlspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/mghio/graphqlspringdemo/GraphqlSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mghio.graphqlspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
